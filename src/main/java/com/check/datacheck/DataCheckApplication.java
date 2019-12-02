d701c9d3cc859486386f5a9553972c89634b9e1b
谢森 <xiesen310@163.com>
2019-12-02 16:38:56 +0800

    <dev> Initial commit

---
+++ src/main/java/com/check/datacheck/DataCheckApplication.java
@@ -0,0 +1,16 @@
+package com.check.datacheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 谢森
+ */
+@SpringBootApplication
+public class DataCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/check/datacheck/DataCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.check.datacheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
