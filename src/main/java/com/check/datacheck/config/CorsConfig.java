1d1a3a53989114b3334925456340ce1703d04a26
xiesen110 <xiesen310@qq.com>
2020-04-05 19:24:15 +0800

    <dev> 解决跨域问题

---
+++ src/main/java/com/check/datacheck/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.check.datacheck.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @Description 安全配置
+ * @className com.check.datacheck.config.CorsConfig
+ * @Author 谢森
+ * @Email xiesen@zork.com.cn
+ * @Date 2020/4/5 17:17
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
+}
