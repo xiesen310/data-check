a7513ebe6a6d62b1facef9d3fa4a747ee9ebdf62
xiesen110 <xiesen310@qq.com>
2020-04-08 21:07:32 +0800

    <dev> 设置 kafka 生产数据不丢失

--- src/main/java/com/check/datacheck/kafka/CustomProducer.java
+++ src/main/java/com/check/datacheck/kafka/CustomProducer.java
@@ -4,14 +4,10 @@ import com.check.datacheck.init.AppProperties;
 import com.check.datacheck.utils.ConfigUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import scala.App;
 
 import java.util.Properties;
 
@@ -38,16 +34,52 @@ public class CustomProducer {
     public CustomProducer() {
         initConfig();
         Properties props = new Properties();
-        props.put("bootstrap.servers", servers);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("batch.size", batchSize);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer");
+
+        /**
+         * 这个参数控制着相同分区内数据发送的批次个数大小，也就是当数据达到 这个size 时，进行数据发送,
+         * 但是并不是数据达不到 size 的值，就不会发送数据，默认是 1048576，即 16k
+         */
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
+
+        // 当数据发送失败时，重试次数设置
+        props.put(ProducerConfig.RETRIES_CONFIG, 5);
+
+        /**
+         * 消息是否发送，不是仅仅通过 batch.size 的值来控制的，实际上是一种权衡策略，即吞吐量和延时之间的权衡
+         * linger.ms 参数就是控制消息发送延时行为的，默认是 0，表示消息需要被立即发送。
+         */
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 100);
+
+        /**
+         * 控制消息发送的最大消息大小，默认是 10485760 字节 即 10Mb
+         */
+        props.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, 10485760);
+
+        /**
+         * 当 producer 发送消息到 broker 时，broker 需要在规定的时间内返回结果，这个时间就是该参数控制的，默认是 30s
+         */
+        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 60000);
+
+        /**
+         * 指定了producer 端用于缓存的缓存区大小，单位是字节，默认是 33554432, 即 32G
+         */
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        /**
+         * 用户控制 生产者的持久性 acks 有3个值，
+         *  0: 表示producer 完全不理睬 broker 的处理结果
+         *  all： 表示发送数据时，broker 不仅会将消息写入到本地磁盘，同时也要保证其他副本也写入完成，才返回结果
+         *  1: 表示发送数据时，broker 接收到消息写入到本地磁盘即可，无需保证其他副本是否写入成功
+         */
+        props.put(ProducerConfig.ACKS_CONFIG, "1");
+
         kafkaProducer = new KafkaProducer<String, String>(props);
     }
 
     /**
      * 初始化 kafka 集群配置
-     * todo 在 构造方法中没有办法去加载 spring 注入的参数
      */
     public void initConfig() {
         servers = ConfigUtils.getString(AppProperties.kafkaServers, "localhost:9092");
